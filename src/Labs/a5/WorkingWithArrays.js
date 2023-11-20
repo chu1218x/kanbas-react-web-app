f72aec1c2d3f40dd9684305ec6c59cd20ba26987
xuanchu jiang <xuanchujiang@gmail.com>
2023-11-20 16:41:59 -0500

    a5 lab url

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -11,7 +11,7 @@ function WorkingWithArrays() {
         completed: false,
     });
 
-    const API = "http://localhost:4000/a5/todos";
+    const API = "https://kanbas-node-server-app-gxqq.onrender.com/a5/todos";
 
     const [id, setId] = useState(1);
 
