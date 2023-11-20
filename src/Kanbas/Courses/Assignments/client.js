73cbe89c906623fc68d69c415e6409b9f4644f0d
xuanchu jiang <xuanchujiang@gmail.com>
2023-11-20 16:37:01 -0500

    assignment url

--- src/Kanbas/Courses/Assignments/client.js
+++ src/Kanbas/Courses/Assignments/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/api/courses";
-const ASSIGNMENT_URL = "http://localhost:4000/api/assignments";
+const COURSES_URL = "https://kanbas-node-server-app-gxqq.onrender.com/api/courses";
+const ASSIGNMENT_URL = "https://kanbas-node-server-app-gxqq.onrender.com/api/assignments";
 
 export const findAssignmentsForCourse = async (courseId) => {
     const response = await axios
