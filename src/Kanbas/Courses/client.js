f1f10365482772a032fbace09416cfc8cc57e972
xuanchu jiang <xuanchujiang@gmail.com>
2023-11-20 16:22:31 -0500

    Dashboard URL

--- src/Kanbas/Courses/client.js
+++ src/Kanbas/Courses/client.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
 export const fetchCourses = async () => {
-    const response = await axios.get("http://localhost:4000/api/courses");
+    const response = await axios.get("https://kanbas-node-server-app-gxqq.onrender.com/api/courses");
     return response.data;
 };
 
 export const fetchCourse = async (id) => {
-    const response = await axios.get(`http://localhost:4000/api/courses/${id}`);
+    const response = await axios.get(`https://kanbas-node-server-app-gxqq.onrender.com/api/courses/${id}`);
     return response.data;
 };
 
 export const deleteCourse = async (id) => {
-    const response = await axios.delete(`http://localhost:4000/api/courses/${id}`);
+    const response = await axios.delete(`https://kanbas-node-server-app-gxqq.onrender.com/api/courses/${id}`);
     return response.data;
 };
 
 export const addCourse = async (course) => {
-    const response = await axios.post("http://localhost:4000/api/courses", course);
+    const response = await axios.post("https://kanbas-node-server-app-gxqq.onrender.com/api/courses", course);
     return response.data;
 }
 
 export const updateCourse = async (course) => {
     const response = await axios.put(
-        `http://localhost:4000/api/courses/${course._id}`,
+        `https://kanbas-node-server-app-gxqq.onrender.com/api/courses/${course._id}`,
         course);
     return response.data;   
 }
